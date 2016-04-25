81cd6304be91622ed9f9728ebf24d84da33c66c5
Roee Sefi <roee.sefi@gmail.com>
2016-04-25 10:22:54 +0300

    Add threat generation in launch file ('config/generate_launch.py')

--- adversarial_coverage/config/generate_launch.py
+++ adversarial_coverage/config/generate_launch.py
@@ -26,6 +26,28 @@ def gen_obstacle_spawn_node(i):
 
     return ret
 
+def gen_threat_xacro_param(i, o):
+    ret = ET.Element("param")
+
+    ret.attrib["name"] = "threat_%s" % i
+    ret.attrib["command"] = "$(find xacro)/xacro.py $(find adversarial_coverage)/objects/threat.urdf.xacro row:=%s col:=%s width:=%s height:=%s level:=%s" % \
+                               (str(float(o["start_row"]) - 10 + 0.5 * float(o["num_rows"])), str(float(o["start_col"]) - 10 + 0.5 * float(o["num_cols"])), \
+                               o["num_rows"], o["num_cols"], o["level"])
+
+    return ret
+
+def gen_threat_spawn_node(i):
+    ret = ET.Element("node")
+
+    ret.attrib["name"]    = "spawn_threat_%s" % i
+    ret.attrib["pkg"]     = "gazebo_ros"
+    ret.attrib["type"]    = "spawn_model"
+    ret.attrib["args"]    = "-urdf -param threat_%s -model threat_%s" % (i, i)
+    ret.attrib["respawn"] = "false"
+    ret.attrib["output"]  = "screen"
+
+    return ret
+
 def main():
     input_file_path = os.path.dirname(__file__)
 
@@ -47,6 +69,13 @@ def main():
         launch.append(p)
         l = gen_obstacle_spawn_node(i)
         launch.append(l)
+
+    for i, threat in input_yaml["threats"].iteritems():
+        p = gen_threat_xacro_param(i, threat)
+        launch.append(p)
+        l = gen_threat_spawn_node(i)
+        launch.append(l)
+
 
     #print etree.tostring(tree, pretty_print=True)
     tree.write(os.path.join(input_file_path, ("../launch/%s.launch" % (input_yaml["id"]))))
